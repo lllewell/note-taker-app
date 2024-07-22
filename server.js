b7b6b860586386a1a84df6368ce3b30973edd682
Alien <lmllewellyn@gmail.com>
2024-07-22 15:23:49 -0400

    got rid of the Develop folder and corrected pathing in server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const savedNotes = require('./Develop/db/db.json');
-const uuid = require('./Develop/helpers/uuid');
+const savedNotes = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const { readAndAppend } = require('./helpers/fsUtils');
 const fs = require('fs');
 
 const app = express();
@@ -16,40 +17,51 @@ app.use(express.static('public'));
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
 
-    const newNote = {
-        title,
-        text,
-        note_id: uuid(),
-    };
+    const { title, text } = req.body;
 
-    const data = fs.readFileSync(savedNotes);
+    if (title && text) {
 
-    const madeNotes = JSON.parse(data);
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
 
-    madeNotes.push(newNote);
+        readAndAppend(newNote, './db/db.json');
 
-    const noteString = JSON.stringify(madeNotes, null, 2);
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
 
-    fs.writeFileSync('./Develop/db/db.json', noteString, (err) => {
-        err
-            ? console.error(err)
-            : console.log(`New note has been added to JSON file`);
-    });
+        console.log(response);
 
-    const response = {
-        status: 'success',
-        body: newNote,
-    };
 
-    console.log(response);
+        const data = fs.readFileSync(savedNotes);
+
+        const madeNotes = JSON.parse(data);
+
+        madeNotes.push(newNote);
+
+        const noteString = JSON.stringify(madeNotes, null, 2);
+
+        fs.writeFileSync('./db/db.json', noteString, (err) => {
+            err
+                ? console.error(err)
+                : console.log(`New note has been added to JSON file`);
+        });
+
+
+        console.log(response);
+    }
 });
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
